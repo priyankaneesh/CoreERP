fb62b1e5f0126bf3e3c0aa811147aa49b2018c15
priyanka <priyankamg12@gmail.com>
2024-12-03 14:30:53 +0530

    dashh

--- CoreERP/CoreERP/obj/Debug/net8.0/CoreERP.AssemblyInfo.cs
+++ CoreERP/CoreERP/obj/Debug/net8.0/CoreERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827625fd460fa189eae5d782d9f7ff5e40bd2ea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de8f818e23be74cdb8006cd48f6258dcf2e6868")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
